74eadb15b611bd515f41cebef84bd650420a387f
karine <karine.fbueno@gmail.com>
2024-12-28 14:55:01 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from '../src/pages/Home/index'
+import { GlobalStyle } from './styles/GlobalStyles';
+
+function App() {
+  
+  return (
+  <>
+    <GlobalStyle/>
+    <Home/>
+  </>  
+  );
+  
+}
+
+export default App;
